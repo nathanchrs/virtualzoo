60803a456ed2eed49c8debfcfb66df61259723b2
Sulley01 <sullivan.alvin@ymail.com>
2017-03-11 16:54:22 +0700

    Implementasi constructor Aves

--- src/Animal/Aves/Aves.cpp
+++ src/Animal/Aves/Aves.cpp
@@ -1,1 +1,9 @@
 #include "Aves.h"
+
+/** @brief Constructor.
+      * Menciptakan Aves yang memiliki skinType "Feather" dan reproduction "Ovipar".
+      */
+Aves::Aves() {
+    reproduction="Ovipar";
+    skinType="Feather";
+}
