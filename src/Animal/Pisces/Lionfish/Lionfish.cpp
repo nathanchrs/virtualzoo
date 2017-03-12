451ab7935d46a78e164cc8a481ec6771c40e7752
Sulley01 <sullivan.alvin@ymail.com>
2017-03-12 13:27:59 +0700

    Created implementation of Barracuda, FrenchAngelFish, Lionfish, Ray, Seahorse

--- src/Animal/Pisces/Lionfish/Lionfish.cpp
+++ src/Animal/Pisces/Lionfish/Lionfish.cpp
@@ -1,9 +1,10 @@
 #include "Lionfish.h"
 
 Lionfish::Lionfish(int _weight) : Pisces("Lionfish"), Carnivore(_weight) {
-    description = "<Lionfish description>"; //TODO description
+    description = "Lionfish is an animal native to the Indo-Pacific. It is characterized by conspicuous warning coloration"
+                  " and venomous spiky fin rays. This ovipar animal has water as its habitat.";
 }
 
 string Lionfish::interact() const {
-    return "<Lionfish interaction>"; // TODO interaction
+    return "Whoa! I thought it was plant. It is lionfish!"; // TODO interaction
 }
